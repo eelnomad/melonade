919575dca2e9881a69e38a3a7ac8ac3f9446b92f
Nomad <bladesofheaven@gmail.com>
2017-07-28 15:27:16 -0400

    Lots of style changes, organized css classes... kinda...

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
 
 <script>
 import 'normalize.css/normalize.css'
+import '../src/css/custom.css'
 
 export default {
   name: 'app'
@@ -13,76 +14,8 @@ export default {
 </script>
 
 <style>
-html, body {
-  width: 100%;
-  height: 100%;
-}
-
-h1 {
-  font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-  font-size: 24px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: 500;
-  line-height: 26.4px;
-}
-h2 {
-  font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-  font-size: 19px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: 500;
-  line-height: 20.9px;
-}
-h3 {
-  font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-  font-size: 14px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: 500;
-  line-height: 15.4px;
-}
-p {
-  font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-  font-size: 14px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: 400;
-  line-height: 20px;
-}
-blockquote {
-  font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-  font-size: 21px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: 400;
-  line-height: 30px;
-}
-pre {
-  font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
-  font-size: 13px;
-  font-style: normal;
-  font-variant: normal;
-  font-weight: 400;
-  line-height: 18.5714px;
-}
-
 #app {
   width: 100%;
   height: 100%;
-}
-
-.background {
-  display: flex;
-  position: relative;
-  z-index: -99;
-  width: 100%;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-
-a {
-  text-decoration: none;
 }
 </style>
--- src/components/BlogType.vue
+++ src/components/BlogType.vue
@@ -2,15 +2,23 @@
      This is the landing page of the website with four quadrants to select what category of posts to look at.-->
 
 <template>
-  <router-link :to="post.type + '/' + post._id" v-on:mouseover.native="mouseOver" v-on:mouseleave.native="mouseOver">
-    <div class="background" :style="{ 'background-image': 'url(' + post.background_image + ')' }">
-      <div id="blog-category" v-if="!active" :style="{ 'color': post.font_preview_color }">
-        <h1>{{post.type}}</h1>
-      </div>
-      <div id="blog-preview" v-else :style="{ 'color': post.font_preview_color }">
-        <h2>{{post.title}}</h2>
-        <h3>{{post.body}}</h3>
-      </div>
+  <router-link :to="post.type + '/' + post._id" v-on:mouseenter.native="mouseOver" v-on:mouseleave.native="mouseOver">
+    <div id="blog-preview" v-show="!active">
+      <span></span>
+      <h1>{{post.type}}</h1>
+      <span></span>
+    </div>
+    <div id="blog-preview" v-show="active">
+      <h2>{{post.title}}</h2>
+      <h3>{{post.body}}</h3>
+      <span></span>
+      <button id="blog-type-button" :to="post.type">
+        <router-link :to="post.type">
+          More {{post.type}}
+        </router-link>
+      </button>
+    </div>
+    <div class="background dim" :style="{ 'background-image': 'url(' + post.background_image + ')' }">
     </div>
   </router-link>
 </template>
@@ -37,22 +45,59 @@ export default {
 #blog-type {
   transition: all .5s ease;
   display: flex;
-  flex: 1 0 auto;
+  flex: 1 1 auto;
+  position: relative;
+  color: white;
+  vertical-align: center;
+  z-index: 1;
 } 
 
 #blog-type:hover {
+  display: flex;
   flex: 99 1 auto;
 }
 
-#blog-category {
-  transition: all .5s ease;
+#blog-preview {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
   padding: 20px 60px;
-  z-index: -1;
 }
 
-#blog-preview {
-  transition: all .5s ease;
-  padding: 60px 60px;
-  z-index: -1;
+#blog-type-button {
+  background-color: transparent;
+  background-repeat: no-repeat;
+  border-style: solid;
+  border-width: 1px;
+  border-bottom-width: 3px;
+  border-color: white;
+  color: white;
+  float: right;
+  outline-width: 1px;
+  outline-color: white;
+  cursor: pointer;
+  z-index: 2;
+}
+
+a {
+  color: white;
+}
+
+#blog-type-button:hover {
+  border-bottom-width: 2px;
+}
+
+.background {
+  z-index: -99;
+  width: 100%;
+  height: 101%;
+  position: absolute;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.dim {
+  filter: brightness(.5);
 }
 </style>
