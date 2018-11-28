9c91bf6218065e7add98aa9dfb2fc095a97b955d
eelnomad <bladesofheaven@gmail.com>
2018-11-27 19:00:44 -0500

    Some positioning changes to sudoku

--- src/components/sudoku/Sudoku.vue
+++ src/components/sudoku/Sudoku.vue
@@ -5,7 +5,7 @@
   <div class="flex-row" id="sudoku">
     <table class="grid flex-row">
       <td v-for="key in grid" class="grid-block">
-        <input v-model="key.value">
+        <input type="number" v-model="key.value" min="1" max="9">
         </td>
       </td>
     </table>
@@ -93,19 +93,23 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   #sudoku {
+    height: 100%;
+    width: 100%;
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    align-items: center;
     /*background-image: url('https://lh3.googleusercontent.com/UqrZHJ0MVurefzgefF9zCzR3cUGkOQY97ZSx6yiZJycfSLNAy9fpK0VzfmPXZiib9JA459-cPcxke699A8Ovy3Nh4bM8xVv5NsatsBL4gyF6Q3Gp0Xn1ZZshnz4MzSmmCRP4vI2g8dqys0jqOUIDzz2B28ia1AYKPd4NN7_gu2e5sLyD-0X2Pbb_Zi6LNIt20nl15dJjuLI-QaMnYH9EjtBp1qPMLwJFG5q5l3tTbOFgA-Tws1rRJ6sfLNYfywnEKtwzt6_C-uqSfdPlzu_0btVpI3aRZz2zfSr13tkJ3mYAVSnRuG_23laoaG4fhOEIaZGqOUPzNVtDyWojZ2ORWu48YoOM_EpxGAWQ_1S2AH51eiqB-wJaPSZVpkdy_RqBz0_oRePLAfkIVNF-DzNg0qfmYfjn1wMkRXkwRwp0xny0oMh6FCpYbfbC1D4rnSWW_nfCE9CbwhOM-oQfDPzqv4Bwwf3DVqCltLROzgjOa_j9bW_23kop1wK80QeXnFCd506eXKqb_GtHmViQ1Z_KU1lSjF36wbsRsA0Dql5YojNBEnMR_ZsJHNNXYM8mwRSEBOxfgzDEEEwE9rKwNem3oerhJfF8WY6vwDCum23B7aPqZckO8jmq1b7dCxGWNUwsJhqjRKX53De50Gun_NxMQqgnf7OV0JQ-5OA=w1980-h1320-no?.jpg');*/
   }
 
   .grid {
     flex-wrap: wrap;
-    height: 85vmin;
-    width: 85vmin;
+    height: 81vmin;
+    width: 81vmin;
   }
 
   .grid-block {
+    box-sizing: border-box;
     height: 9vmin;
     width: 9vmin;
   }
@@ -113,7 +117,12 @@ export default {
   .grid-block > input {
     height: 100%;
     width: 100%;
-    font-size: 7vmin;
+    font-size: 2vmin;
     text-align: center;
+  }
+  input[type=number]::-webkit-inner-spin-button, 
+  input[type=number]::-webkit-outer-spin-button { 
+    -webkit-appearance: none; 
+    margin: 0; 
   }
 </style>
